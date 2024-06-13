1131088674bae97d0dc4607a2a0e4dffd88623ce
Artem Sharanin <sharan.a@mail.ru>
2024-06-13 20:21:23 +0300

    Initial commit

---
+++ diag_gen_script.sql
@@ -0,0 +1,44 @@
+SELECT res FROM (
+WITH 
+sch_name AS (SELECT 'diag' AS sn), -- Указать название вашей схемы
+sch_name_l AS (SELECT LENGTH(sn) + 1 AS l FROM sch_name),
+
+t AS (
+SELECT 
+    c.table_name,
+    c.column_name,
+    c.udt_name,
+    CASE 
+    	WHEN description LIKE (SELECT '%'||sn||'.%' FROM sch_name)
+			THEN SUBSTRING(description, POSITION((SELECT sn||'.' FROM sch_name) IN description) + (SELECT l FROM sch_name_l))
+		ELSE '0'
+	END AS subst
+FROM information_schema.columns c
+LEFT JOIN pg_catalog.pg_description d ON d.objsubid = c.ordinal_position 
+									AND d.objoid = c.table_name::regclass 
+WHERE c.table_schema = (SELECT sn FROM sch_name)
+ORDER BY c.table_schema, c.table_name, c.ordinal_position),
+
+ins AS (
+SELECT table_name, STRING_AGG(column_name || ' ' || udt_name, ' ') AS insd
+FROM t
+GROUP BY table_name)
+
+SELECT * 
+FROM (VALUES ('erDiagram', 1, 'a')) er (res, rn, tn)
+
+UNION 
+SELECT ' ' || table_name || ' { ' || insd || ' }' AS res, 2 AS rn, table_name
+FROM ins
+
+
+UNION
+SELECT *
+FROM (VALUES (' ', 3, 'a')) sp (res, rn, tn)
+
+UNION
+SELECT ' ' || table_name || ' }o--|| ' || subst || ' : ""' AS res, 4 AS rn, table_name
+FROM t
+WHERE subst <> '0'
+) tt
+ORDER BY rn, tn
---
+++ edited_diag.md
@@ -0,0 +1,19 @@
+```mermaid
+erDiagram
+ address { id int8 gar uuid no_gar varchar }
+ customer { id int8 person_id int8 }
+ customer_order { id int8 customer_id int8 employee_id int8 order_items jsonb created_at timestamp updated_at timestamp }
+ employee { id int8 person_id int8 }
+ person { id int8 user_id int8 first_name text last_name text full_name tsvector }
+ person_address { id int8 person_id int8 address_id int8 }
+ product { id int8 code uuid name varchar }
+ user_t { id int8 login varchar main_email varchar is_del bool extra jsonb }
+ 
+ customer |o--|| person : ""
+ customer_order }o--|| product : ""
+ customer_order }o--|| customer : ""
+ customer_order }o--o| employee : ""
+ employee |o--|| person : ""
+ person |o--o| user_t : ""
+ person_address }o--|| address : ""
+ person_address }o--|| person : ""
